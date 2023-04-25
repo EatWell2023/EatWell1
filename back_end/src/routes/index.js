ba180cd3355069f424c94e741fc008dc0499e13e
EatWell <128539368+EatWell2023@users.noreply.github.com>
2023-04-25 13:51:24 +0530

    Delete back_end directory

--- back_end/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const userControl=require('../controller/user_controller');
-router.post('/register',userControl.registerUser);
-router.post('/login',userControl.loginUser)
-
-module.exports=router;
